59cf42c5817ed58a5331fafed439e2d1b28ab5f2
Marion101200 <124160461+Marion101200@users.noreply.github.com>
2025-02-05 16:03:31 +0100

    datalist integré

--- header.php
+++ header.php
@@ -49,24 +49,72 @@ if (session_status() == PHP_SESSION_NONE) {
         <?php
         if (isset($_SESSION['email'])) {
           echo '<li><a href="favoris.php"><i class=\'bx bxs-heart\' ></i></a></li>';
-          echo '<li><a href="historique_commandes.php">Historique</a></li>';
           echo "<li><a href=\"compte.php\">Compte</li>";
           echo "<li><a href=\"logout.php\">Logout &nbsp; <span class=\"glyphicon glyphicon-log-in\"></span></a></li>";
         } else {
           echo "<li><a href=\"login.php\">Login &nbsp; <span class=\"glyphicon glyphicon-log-in\"></span></a></li>";
         }
         ?>
-    </ul>
-    <form class="navbar-form navbar-left" action="research.php" method="get">
-      <div class="form-group">
-        <input type="search" name="query" class="form-control" placeholder="rechercher..." required style="width: 440px">
-      </div>
-      <button type="submit" class="btn btn-default"><i class='bx bx-search-alt-2' ></i>
-      </button>
-    </form>
-  </div>
-  </div>
-</nav>
+      </ul>
+      <form class="navbar-form navbar-left" action="research.php" method="get">
+        <div class="form-group">
+        <input type="search" name="query" id="products" class="form-control" placeholder="rechercher..." required style="width: 440px" list="datalist">
+<datalist id="datalist"></datalist>
+        </div>
+        <button type="submit" class="btn btn-default"><i class='bx bx-search-alt-2'></i>
+        </button>
+      </form>
+    </div>
+    </div>
+  </nav>
+  <script>
+$(document).ready(function() {
+    let inputRecherche = $('input[name="query"]');
+
+    inputRecherche.on('input', function() {
+        var valeur_saisie = $(this).val();
+        if (valeur_saisie.length > 1) {
+            $.ajax({
+                url: 'recup_produit.php',
+                method: 'GET',
+                dataType: 'json',
+                data: { search: valeur_saisie },
+                success: function(data) {
+
+                    $('#datalist').empty(); // On vide la liste avant d’ajouter de nouvelles options
+                    
+                    let titresAjoutes = new Set();
+                    $.each(data, function(index, produit) {
+                        if (!titresAjoutes.has(produit.titre)) {
+                            $('#datalist').append(
+                                $('<option>', { value: produit.titre })
+                            );
+                            titresAjoutes.add(produit.titre);
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    // Vider la datalist une fois qu'un jeu est sélectionné
+    inputRecherche.on('change', function() {
+        setTimeout(() => {
+            $('#datalist').empty(); // Efface la liste après la sélection
+        }, 50); // Petit délai pour éviter la suppression immédiate avant la sélection
+    });
+});
+
+
+
+
+  </script>
 </body>
 
 </html>
+
+
+
+
+
+
