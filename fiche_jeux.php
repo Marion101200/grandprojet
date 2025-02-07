62080fdf2b19fefa1d2a51bd77e185dd9f4bea4a
Marion101200 <124160461+Marion101200@users.noreply.github.com>
2025-02-07 16:43:56 +0100

    du style à été ajouter

--- fiche_jeux.php
+++ fiche_jeux.php
@@ -7,8 +7,8 @@ error_reporting(E_ALL);
 include 'pdo.php';
 include 'ajouter_avis.php';
 require_once("connexion.php");
-  $connexion = getConnexion();
-  $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$connexion = getConnexion();
+$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
@@ -62,10 +62,12 @@ $avisStmt = $connexion->prepare("SELECT * FROM avis WHERE jeux_titre = ? ORDER B
 $avisStmt->execute([$jeux['titre']]);
 $avis = $avisStmt->fetchAll();
 
-$email = $_SESSION['email'];
-$stmt = $connexion->prepare("SELECT * FROM clients WHERE email = ?");
+if(isset($_SESSION['email'])){
+    $email = $_SESSION['email'];
+    $stmt = $connexion->prepare("SELECT * FROM clients WHERE email = ?");
 $stmt->execute([$email]);
-$clients = $stmt->fetch(PDO::FETCH_ASSOC);
+$clients = $stmt->fetch(PDO::FETCH_ASSOC);   
+}
 
 include 'header.php';
 ?>
@@ -74,96 +76,94 @@ include 'header.php';
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= htmlspecialchars($jeux['titre']); ?></title>
-  <link rel="stylesheet" href="fiche_jeux.css"> <!-- Votre CSS -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlspecialchars($jeux['titre']); ?></title>
+    <link rel="stylesheet" href="fiche_jeux.css"> <!-- Votre CSS -->
 </head>
 
 <body>
-  <div class="container">
-    <div class="jeux-item">
-      <img class="images_jeux" src="<?php echo htmlspecialchars($jeux['images']); ?>" alt="<?php echo htmlspecialchars($jeux['titre']); ?>">
-      <h1><?= htmlspecialchars($jeux['titre']); ?></h1>
-      <p class="description">
-        <strong>Catégorie : </strong>
-      <h6 class="details"><?php echo htmlspecialchars($jeux['categorie']); ?></h6>
-      <strong>Description : </strong>
-      <h6 class="details"><?php echo htmlspecialchars($jeux['description']); ?></h6>
-      <strong>Date de sortie : </strong>
-      <h6 class="details"><?php echo htmlspecialchars($jeux['date']); ?></h6>
-      </p>
-      <div class="prix_ajout">
-        <h3 class="prix"><?php echo htmlspecialchars($jeux['prix']); ?> €</h3>
+    <div class="container">
+        <div class="jeux-item">
+            <img class="images_jeux" src="<?php echo htmlspecialchars($jeux['images']); ?>" alt="<?php echo htmlspecialchars($jeux['titre']); ?>">
+            <h1><?= htmlspecialchars($jeux['titre']); ?></h1>
+            <p class="description">
+                <strong>Catégorie : </strong>
+            <h6 class="details"><?php echo htmlspecialchars($jeux['categorie']); ?></h6>
+            <strong>Description : </strong>
+            <h6 class="details"><?php echo htmlspecialchars($jeux['description']); ?></h6>
+            <strong>Date de sortie : </strong>
+            <h6 class="details"><?php echo htmlspecialchars($jeux['date']); ?></h6>
+            </p>
+            <div class="prix_ajout">
+                <h3 class="prix"><?php echo htmlspecialchars($jeux['prix']); ?> €</h3>
 
-        <form class="favoris" method="post">
-          <input type="hidden" name="jeux_id" value="<?php echo htmlspecialchars($jeux['id']); ?>">
-          <button class="ajout-panier" type="submit" name="add-to-cart">Ajouter au panier</button>
-          <input type="hidden" name="favori_id" value="<?php echo htmlspecialchars($jeux['id']); ?>">
-          <button class="ajout-favori" type="submit" name="add-to-favorites">
-            ❤️
-          </button>
-        </form>
-      </div>
-    </div>
-  </div>
-  <a href="panier.php" class="see_cart"><i class='bx bxs-cart'></i>&nbsp;Voir le panier&nbsp;<i class='bx bxs-cart'></i></a>
-
-  <h2>Donnez votre avis</h2>
-  <?php if(isset($_SESSION['nom'])): ?>
-    <form action="" method="post">
-        <input type="hidden" name="jeux_titre" value="<?php echo $jeux['titre']; ?>">
-        <input type="hidden" name="nom" value="<?php echo $clients['nom']; ?>">
-      <input type="hidden"  name="note" id="note" value="0">
-
-      <label>Commentaire: </label>
-      <textarea name="commentaire" required></textarea>
-      <button type="submit">Envoyer</button>
-    </form>
-    <?php else: ?>
-      <p><a href="login.php">Connectez-vous</a> pour laisser un avis. </p>
-      <?php endif; ?>
-
-      <div class="star-rating">
-<?php for($i = 1; $i <= 5; $i++): ?>
-  <span class="star" data-value="<?= $i; ?>">★</span>
-
-  <?php endfor; ?>
-
-      </div>
-    
-      <h2>Avis des utilisateurs</h2>
-      <?php foreach($avis as $a): ?>
-        <div class="avis">
-          <strong><?= htmlspecialchars($a['nom']); ?></strong>
-          <span><?= str_repeat('⭐', $a['note']); ?></span>
-          <p><?= nl2br(htmlspecialchars($a['commentaire'])); ?></p>
-          <small>Posté le <?= $a['date_ajout']; ?></small>
+                <form class="favoris" method="post">
+                    <input type="hidden" name="jeux_id" value="<?php echo htmlspecialchars($jeux['id']); ?>">
+                    <button class="ajout-panier" type="submit" name="add-to-cart">Ajouter au panier</button>
+                    <input type="hidden" name="favori_id" value="<?php echo htmlspecialchars($jeux['id']); ?>">
+                    <button class="ajout-favori" type="submit" name="add-to-favorites">
+                        ❤️
+                    </button>
+                </form>
+            </div>
         </div>
-        <?php endforeach; ?>
-  <div class="retour-accueil">
-  <a href="accueil.php"><i class='bx bxs-invader'></i>&nbsp;Retour à la liste de jeux&nbsp;<i class='bx bxs-invader'></i></a>
-</div>
+    </div>
+    <a href="panier.php" class="see_cart"><i class='bx bxs-cart'></i>&nbsp;Voir le panier&nbsp;<i class='bx bxs-cart'></i></a>
 
-<script>
-    document.addEventListener('DOMContentLoaded', function (){
-const stars = document.querySelectorAll(".star");
-const noteInput = document.getElementById("note");
+    <h2 class="donner_avis"><i class='bx bxs-message-dots'></i>&nbsp;Donnez votre avis&nbsp;<i class='bx bxs-message-dots'></i></h2>
+    <?php if (isset($_SESSION['nom'])): ?>
+        <form id="commentaire" method="post">
+            <input type="hidden" name="jeux_titre" value="<?php echo $jeux['titre']; ?>">
+            <input type="hidden" name="nom" value="<?php echo $clients['nom']; ?>">
+            <input type="hidden" name="note" id="note" value="0">
+            <label><i class='bx bxs-message-dots'></i>&nbsp;Commentaire: &nbsp;<i class='bx bxs-message-dots'></i></label>
 
-stars.forEach(star => {
-    star.addEventListener("click", function (){
-        let value = this.getAttribute("data-value");
-        noteInput.value = value;
+            <textarea name="commentaire" required></textarea>
+            <div class="star-rating">
+                <?php for ($i = 1; $i <= 5; $i++): ?>
+                    <span class="star" data-value="<?= $i; ?>">★</span>
+
+                <?php endfor; ?>
+            </div>
+                <button id="button_avis" type="submit">Envoyer</button>
+        </form>
+        
+    <?php else: ?>
+        <p class="avis_connexion"><a class="avis_connexion" href="login.php"><i class='bx bx-user'></i>&nbsp;Connectez-vous</a> pour laisser un avis.&nbsp;<i class='bx bx-user'></i></p>
+    <?php endif; ?>
+
+    <h2><i class='bx bxs-star'></i>&nbsp;Avis des utilisateurs &nbsp;<i class='bx bxs-star'></i></h2>
+    <?php foreach ($avis as $a): ?>
+        <div class="avis">
+            <strong><?= htmlspecialchars($a['nom']); ?></strong> <br>
+            <span><?= str_repeat('⭐', $a['note']); ?></span>
+            <p><?= nl2br(htmlspecialchars($a['commentaire'])); ?></p>
+            <small>Posté le <?= $a['date_ajout']; ?></small>
+        </div>
+    <?php endforeach; ?>
+    <div class="retour-accueil">
+        <a href="accueil.php"><i class='bx bxs-invader'></i>&nbsp;Retour à la liste de jeux&nbsp;<i class='bx bxs-invader'></i></a>
+    </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const stars = document.querySelectorAll(".star");
+            const noteInput = document.getElementById("note");
+
+            stars.forEach(star => {
+                star.addEventListener("click", function() {
+                    let value = this.getAttribute("data-value");
+                    noteInput.value = value;
 
 
-        stars.forEach(s => s.classList.remove("selected"));
-        for(let i = 0; i < value; i++){
-            stars[i].classList.add("selected");
-        }
-    })
-})
-    }
-    );
+                    stars.forEach(s => s.classList.remove("selected"));
+                    for (let i = 0; i < value; i++) {
+                        stars[i].classList.add("selected");
+                    }
+                })
+            })
+        });
     </script>
 </body>
 
