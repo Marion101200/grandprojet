3f8b045980ffa377c2c70d6de99da3e714a0435e
Marion101200 <124160461+Marion101200@users.noreply.github.com>
2025-02-10 11:17:10 +0100

    mettre en tableau les résultats

--- API.php
+++ API.php
@@ -9,9 +9,12 @@ $connexion = getConnexion();
 $bd = $connexion->query("SHOW TABLES")->fetchAll(PDO::FETCH_COLUMN);
 
 
+$data = [];
 
 foreach($bd AS $bdResult){
     $sql = $connexion->query("SELECT * FROM {$bdResult}");
-    $stockresult = $sql->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($stockresult);
+    $data[$bdResult] = $sql->fetchAll(PDO::FETCH_ASSOC);
+    // $stockresult = $sql->fetchAll(PDO::FETCH_ASSOC);
 }
+echo json_encode($data);
+exit;
