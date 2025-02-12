692a4a3817e879c5836ac078c5963b3c4c7d3278
Marion101200 <124160461+Marion101200@users.noreply.github.com>
2025-02-12 15:56:15 +0100

    stripe a jour

--- payement.php
+++ payement.php
@@ -1,12 +1,11 @@
 <?php
 require 'vendor/autoload.php';
-$total_price = $_SESSION['total_price'];
 
 
 \Stripe\Stripe::setApiKey('sk_test_51QDpTaFohOKPT3SHP25LuNE9IlSaLTUWYlNEYppRQVWdaDZyQ3QTmOvAqSYjgnrkE9izsHLUhw9dpnQH0FWSbACw00lzBF8qwn');
 
 $paymentIntent = \Stripe\PaymentIntent::create([
-    'amount' => $total_price * 100,
+    'amount' =>  1000,
     'currency' => 'usd',
 ]);
 
