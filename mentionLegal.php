5f7ba116898a17890ab576a320620651ac800010
Marion101200 <124160461+Marion101200@users.noreply.github.com>
2025-02-21 11:05:40 +0100

    ajout de la mention légal fini

--- mentionLegal.php
+++ mentionLegal.php
@@ -13,62 +13,77 @@ include 'header.php';
 ?>
 
 <h1 class="mention">Mentions Légales</h1>
-
-<p class="legal">
   <h2>  1. Éditeur du site: </h2>
-     L'Univers des Jeux
+  <p class="legal">
+     L'Univers des Jeux <br>
      Forme juridique : Auto-entrepreneur
+     <br>
      Siège social : Paris
+     <br>
      SIRET : 155347964213679
+     <br>
      RCS :138796274896
+     <br>
      Email de contact: universdesjeux@hotmail.com
+     <br>
      Téléphone : 0606060606
 </p>
-     <p class="legal">
    <h2> 2. Directeur de la publication:</h2>
+   <p class="legal">
     Nom et prénom du responsable : Marion 
+    <br>
     Email de contact : admin@hotmail.com
      </p>
-     <p class="legal">
+     
     <h2> Hébergeur du site:</h2>
+    <p class="legal">
     Nom de l'hébergeur : OVH
+    <br>
     Adresse : Paris
+    <br>
     Téléphone : 0606060606
      </p>
-     <p class="legal">
+     
    <h2> 4. Propriété intellectuelle:</h2>
+   <p class="legal">
     L'ensemble du contenu du site L'Univer des Jeux - [http://localhost/grandprojet-main/grandprojet] incluant, de façon non
     limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en
     forme sont la propriété exclusive de L'Univers des Jeux à l'exception des marques, logos ou contenus appartenant
     à d'autres sociétés partenaires ou auteurs.
-
+    <br>
+    <br>
     Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces
     différents éléments est strictement interdite sans l'accord express écrit de L'Univers des Jeux.
      </p>
-     <p class="legal">
+     
 
     <h2>5. Protection des données personnelles:</h2>
+    <p class="legal">
         Les informations recueillies sur le site sont enregistrées dans un fichier
     informatisé par L'Univers des Jeux pour la gestion des commandes, du service client et, le cas échéant, pour des
     actions de communication et de marketing.
-
+    <br>
+    <br>
     Conformément au Règlement Général sur la Protection des Données (RGPD), vous disposez d'un droit d'accès, de
     rectification, de suppression et d'opposition aux informations vous concernant. Pour exercer ces droits, veuillez
     nous contacter à l'adresse suivante : unversdesjeux@hotmail.com.
      </p>
-     <p class="legal">
+     
     <h2>6. Responsabilité:</h2>
+    <p class="legal">
     L'Univers des Jeux s'efforce d'assurer au mieux l'exactitude et la mise à jour des informations
     diffusées sur le site, mais ne peut garantir l'absence d'erreurs ou d'omissions. En conséquence, l'utilisateur
     reconnaît utiliser ces informations sous sa responsabilité exclusive.
      </p>
-     <p class="legal">
+    
     <h2>7. Liens hypertextes:</h2>
+    <p class="legal">
     Le site L'Univers des Jeux peut contenir des liens hypertextes vers d'autres sites. Cependant, L'Univers des Jeux ne peut être tenue responsable du contenu de ces sites externes ni des éventuels préjudices qu'ils
     pourraient engendrer.
      </p>
-     <p class="legal">
+     
    <h2> 8. Droit applicable et juridiction compétente:</h2>
+   <p class="legal">
     Les présentes mentions légales sont régies par le droit français. En
     cas de litige, et après échec de toute tentative de recherche d'une solution amiable, les tribunaux français seront
     seuls compétents.
